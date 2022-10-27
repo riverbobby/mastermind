ffa4e4b37912a5dba58b690d13dfd9438ada6508
Justin Townley <justin.d.townley@gmail.com>
2022-10-27 11:13:50 -0700

    Add main program file and scaffolding for classes

---
+++ mastermind.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+# class for ascii board
+class Board
+  def initialize
+    
+  end
+end
+
+# class for player
+class Player
+  def initialize
+    
+  end
+end
+
+# class for individual move
+class Move
+  def initialize
+    
+  end
+end
+
+# class for computer ai
+class Computer
+  def initialize
+    
+  end
+end
+
+# class for game
+class Game
+  def initialize
+    
+  end
+end
